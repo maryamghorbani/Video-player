4b9c8d528e4fdc5545c387197a48db51d36b98ba
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-05-04 18:32:39 +0430

    check click listener works good

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 let playerArea = document.querySelector('.myplayer');
 let media = playerArea.querySelector('video');
-let controls = playerArea.querySelector('.myplayer_controls');
+let controls = playerArea.querySelector('.myplayer__controls');
 
-lef play = controls.querySelector('.play');
-lef rwind = controls.querySelector('.rwind');
-lef fwd = controls.querySelector('.forward');
-// lef volume = controls.querySelector('.volume');
-// lef fullscreen = controls.querySelector('.fullscreen');
+let play = controls.querySelector('.play');
+let rwd = controls.querySelector('.rewind');
+let fwd = controls.querySelector('.forward');
+// let volume = controls.querySelector('.volume');
+// let fullscreen = controls.querySelector('.fullscreen');
 
 
+play.addEventListener('click', function(){
+    console.log('play video');
+})
