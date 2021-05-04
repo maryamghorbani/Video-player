762aaa6571c19a5b6bf32cedf78f1f3feb24578e
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-05-04 17:48:39 +0430

    select elements

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let playerArea = document.querySelector('.myplayer');
+let media = playerArea.querySelector('video');
+let controls = playerArea.querySelector('.myplayer_controls');
+
+lef play = controls.querySelector('.play');
+lef rwind = controls.querySelector('.rwind');
+lef fwd = controls.querySelector('.forward');
+// lef volume = controls.querySelector('.volume');
+// lef fullscreen = controls.querySelector('.fullscreen');
+
+
